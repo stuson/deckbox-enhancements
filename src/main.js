0f6c1065623291dbbaf626d4e636284ccce9f4fc
Sam Tuson <sam.tuson@hotmail.co.uk>
2021-12-12 00:10:57 +0000

    Add test log

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 require("./style.css");
 
+console.log("Deckbox enchancements!");
+
 const injectAutocompleteFinished = () => {
     Tcg.ui.ImportAddCardAdvanced.prototype._afterUpdate = inject(
         Tcg.ui.ImportAddCardAdvanced.prototype._afterUpdate,
